e5d12669f53b5b491795424aa5107a3a10b3ff7f
Pınar Suvaçoğlu <pinarsuvac@gmail.com>
2021-12-05 13:44:26 +0300

    multer added for file upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# [File Metadata Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice)
+
+## Usage
+
+Install all modules listed as dependencies in package.json
+
+```
+npm i
+
+```
+
+Start project with
+
+```
+nodemon server.js
+
+```
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const cors = require('cors');
+require('dotenv').config()
+
+const app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+    res.json({
+        name: req.file.originalname,
+        type: req.file.mimetype,
+        size: req.file.size
+    });
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log('Your app is listening on port ' + port)
+});
